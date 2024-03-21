4c6133e70fd652e36ca42f846a37b75e3e005860
Amanda Martin <amartin95@fordham.edu>
2024-03-20 21:15:38 -0400

    reorg

--- src/guimap/ImperialGUI.java
+++ src/guimap/ImperialGUI.java
@@ -5,7 +5,7 @@ import staticclasses.*;
 public class ImperialGUI extends GUI
 {
     @Override
-    public Road CreateRoad(string name, double locx, double locy, double len, Heading hdg)
+    public Road CreateRoad(String name, double locx, double locy, double len, Heading hdg)
     {
         return new Road(name, locx / Constants.MetersToMiles, locy / Constants.MetersToMiles, len / Constants.MetersToMiles, hdg);
     }
